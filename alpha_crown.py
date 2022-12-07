6d7ae082da29369838cf5d96dc26832075ace65e
Suhas Kotha <kotha.suhas@gmail.com>
2022-12-06 22:15:25 -0500

    quick refactor

--- alpha_crown.py
+++ alpha_crown.py
@@ -1,18 +1,11 @@
-from tkinter import HIDDEN
-
 import gurobipy as gp
 from gurobipy import GRB
 import numpy as np
 import scipy.sparse as sp
-import math
 import torch
-from torch.autograd import Variable
 from tqdm import tqdm
 
 import core.trainer as trainer
-import core.data as data
-
-import matplotlib.pyplot as plt
 
 from tqdm import tqdm as tqdm
 
@@ -25,8 +18,6 @@ class args():
 trainer = trainer.Trainer(args())
 trainer.load_model("log/11-02-16:50:02--TEST-3L/weights-last.pt") # 200 200 3
 trainer.model.eval()
-
-c = torch.Tensor([-0.2326, -1.6094])
 
 def initialize(model, h, L):
     weights = [None]
@@ -94,14 +85,12 @@ def get_bias_lbs(A, lbs, ubs, L):
 
     return bias_lbs
 
-def Omega(end, start, weights, biases, gamma, D, L):
-    assert end >= start
-    if end == start:
-        return torch.eye(biases[start].size(0))
-    elif end == L:
-        return gamma * weights[end].matmul(D[end - 1]).matmul(Omega(end - 1, start, weights, biases, gamma, D, L))
-    else:
-        return weights[end].matmul(D[end - 1]).matmul(Omega(end - 1, start, weights, biases, gamma, D, L))
+def init_Omega(weights, biases, gamma, D, L):
+    def Omega(end, start):
+        assert end >= start
+        if end == start: return torch.eye(biases[start].size(0))
+        return (gamma if end == L else 1) * weights[end].matmul(D[end - 1]).matmul(Omega(end - 1, start))
+    return Omega
 
 L = 3
 
@@ -111,10 +100,7 @@ h[2][0] = 1
 
 gamma, alphas, weights, biases, lbs, ubs = initialize(trainer.model, h, L)
 
-# c = torch.zeros(200)
-# c[0] = 1
-
-def get_obj(gamma, alphas, lbs, ubs, layeri=0):
+def get_obj(gamma, alphas, lbs, ubs, c, layeri=0):
     A, D = get_diagonals(weights, lbs, ubs, gamma, alphas, L)
     bias_lbs = get_bias_lbs(A, lbs, ubs, L)
 
@@ -124,22 +110,26 @@ def get_obj(gamma, alphas, lbs, ubs, layeri=0):
     def weights_scaled(i):
         return (weights[i] if i < L else gamma * weights[L])
 
-    a_crown = (Omega(L, layeri + 1, weights, biases, gamma, D, L).matmul(weights[layeri + 1]))
-    c_crown = sum([Omega(L, i, weights, biases, gamma, D, L).matmul(biases_scaled(i)) for i in range(layeri + 1, L + 1)]) \
-            + sum([Omega(L, i, weights, biases, gamma, D, L).matmul(weights_scaled(i)).matmul(bias_lbs[i - 1]) for i in range(layeri + 2, L + 1)])
+    Omega = init_Omega(weights, biases, gamma, D, L)
+
+    a_crown = Omega(L, layeri + 1).matmul(weights[layeri + 1])
+    c_crown = sum([Omega(L, i).matmul(biases_scaled(i)) for i in range(layeri + 1, L + 1)]) \
+            + sum([Omega(L, i).matmul(weights_scaled(i)).matmul(bias_lbs[i - 1]) for i in range(layeri + 2, L + 1)])
 
     p = 0.9
     thresh = np.log(p / (1 - p))
     x_0 = (ubs[layeri] + lbs[layeri]) / 2.0
-    eps = 2.0 if layeri == 0 else 4.0
+    eps = (ubs[layeri] - lbs[layeri]) / 2.0
 
-    return -torch.norm(c + a_crown, p=1) * eps + (c + a_crown).matmul(x_0) + c_crown + gamma * thresh 
+    return -torch.abs(c + a_crown).squeeze(0).dot(eps) + (c + a_crown).matmul(x_0) + c_crown + gamma * thresh 
 
-optim = torch.optim.SGD([gamma, alphas[1], alphas[2]], lr=0.001, momentum=0.9)
+optim = torch.optim.SGD([gamma, alphas[1], alphas[2]], lr=0.01, momentum=0.9)
+layeri = 1
 
-for _ in range(200):
-    optim.zero_grad(set_to_none=True)
-    loss = -get_obj(gamma, alphas, lbs, ubs, layeri=0)
+for _ in range(20):
+    optim.zero_grad()
+    c = torch.Tensor([-0.2326, -1.6094]) if layeri == 0 else torch.Tensor([1 if i == 0 else 0 for i in range(200)])
+    loss = -get_obj(gamma, alphas, lbs, ubs, c, layeri=layeri)
     print(-loss.item(), gamma.item(), alphas[2][0].item())
     loss.backward()
     optim.step()
