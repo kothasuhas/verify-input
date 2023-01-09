d353330b9b879d9c12bc01ca11b44ab2fb72dedc
Suhas Kotha <kotha.suhas@gmail.com>
2023-01-09 14:33:39 -0800

    seperate gamma for each constraint, simplify network

--- crown/model_utils.py
+++ crown/model_utils.py
@@ -25,6 +25,32 @@ def _encode_constraints(model: trainer.nn.Sequential, H: torch.Tensor, d: torch.
 
     return model, H, d
 
+def _simplify_network(all_layers):
+    new_all_layers = [all_layers[0]]
+    for layer in all_layers[1:]:
+        if (type(layer) is not torch.nn.Linear) and (type(layer) is not torch.nn.ReLU):
+            continue
+        elif (type(layer) is torch.nn.Linear) and (type(new_all_layers[-1]) is torch.nn.Linear):
+            # We can fold together those two layers
+            prev_layer = new_all_layers.pop()
+
+            joint_weight = torch.mm(layer.weight.data, prev_layer.weight.data)
+            if prev_layer.bias is not None:
+                joint_bias = layer.bias.data + torch.mv(layer.weight.data, prev_layer.bias.data)
+            else:
+                joint_bias = layer.bias.data
+
+            joint_out_features = layer.out_features
+            joint_in_features = prev_layer.in_features
+
+            joint_layer = torch.nn.Linear(joint_in_features, joint_out_features)
+            joint_layer.bias.data.copy_(joint_bias)
+            joint_layer.weight.data.copy_(joint_weight)
+            new_all_layers.append(joint_layer)
+        else:
+            new_all_layers.append(layer)
+    return torch.nn.Sequential(*new_all_layers)
+
 def load_model(model_name, file_name, H: torch.Tensor, d: torch.Tensor, stack_n_times: int = 1) -> trainer.nn.Sequential:
     class args():
         def __init__(self):
@@ -36,10 +62,9 @@ def load_model(model_name, file_name, H: torch.Tensor, d: torch.Tensor, stack_n_
     t.load_model(file_name)
     t.model.eval()
 
-    model = t.model * stack_n_times
+    model = _simplify_network(t.model * stack_n_times)
 
-    if H.size(0) > 1:
-        model, H, d = _encode_constraints(model, H, d)
+    # if H.size(0) > 1: model, H, d = _encode_constraints(model, H, d)
     return model, H, d
 
 
