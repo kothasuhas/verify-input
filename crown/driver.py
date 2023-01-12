Improved branching, fixed numerical issues

crown/driver.py
@@ -12,8 +12,8 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 from .model_utils import get_num_layers, get_num_neurons, get_direction_layer_pairs
-from .lp import get_optimized_grb_result, get_triangle_grb_model, get_optimal_grb_model
-from .crown import initialize_all, optimize_bound, ApproximatedInputBound, InputBranch
+from .lp import get_optimized_grb_result, get_triangle_grb_model
+from .crown import initialize_all, optimize_bound, ApproximatedInputBound, ExcludedInputRegions, InputBranch
 from .plot_utils import plot2d
 
 def optimize(model, H, d, num_cs, input_lbs, input_ubs, num_iters, perform_branching=True, contour=True):
@@ -26,10 +26,15 @@ def optimize(model, H, d, num_cs, input_lbs, input_ubs, num_iters, perform_branc
     # based on https://math.stackexchange.com/a/4388888/50742
     cs = [[np.cos(2*np.pi*t / num_cs), np.sin(2*np.pi*t / num_cs)] for t in range(num_cs)]
     for i, c in enumerate(cs):
-        if c[1] == 0:  # the plotting will divide by c[1]
-            cs[i] = [c[0], 0.0001]
+        # very small values can cause numerical instability/inf/nans during plotting
+        if np.abs(c[0]) < 0.0001:
+            cs[i][0] = 0.0001
+        if np.abs(c[1]) < 0.0001:
+            cs[i][1] = 0.0001
 
     approximated_input_bounds: List[ApproximatedInputBound] = []
+    excluded_input_regions: List[ExcludedInputRegions] = []
+
 
     def get_initial_input_branch(model, H, d):
         resulting_lbs, resulting_ubs, params_dict, weights, biases = initialize_all(model=model, input_lbs=torch.Tensor(input_lbs), input_ubs=torch.Tensor(input_ubs), H=H, d=d)
@@ -37,15 +42,23 @@ def optimize(model, H, d, num_cs, input_lbs, input_ubs, num_iters, perform_branc
         return initial_input_branch
 
     branches = [get_initial_input_branch(model, H, d)]
-    if perform_branching:
-        branches += branches[0].split()
 
     plot_number = 0
     num_layers = get_num_layers(model)
-    for branch in tqdm(branches, desc="Input Branches"):
+    while True:
+        if len(branches) == 0:
+            break
+        branch = branches[0]
+        branches = branches[1:]
+
+        abort = False
+        for layeri in range(get_num_layers(model)):
+            if torch.any(branch.resulting_lbs[layeri] > branch.resulting_ubs[layeri]):
+                tqdm.write(f"[WARNING] Input area of the next branch is empty. That's either a bug, or this input region has no intersection with the target area")
+                abort = True  # we'll skip the optimizations and jump to the end of this loop where the current input region is marked as unreachable
+
         pbar = tqdm(range(num_iters), leave=False)
         last_b = []
-        abort = False
         pending_approximated_input_bounds: List[ApproximatedInputBound] = []
         for _ in pbar:
             if abort:
@@ -63,7 +76,7 @@ def optimize(model, H, d, num_cs, input_lbs, input_ubs, num_iters, perform_branc
                     {'params': alphas[1:]},
                 ], lr=3.0, momentum=0.9, maximize=True)
                 for _ in range(10):
-                    optim.zero_grad()
+                    optim.zero_grad(set_to_none=True)
                     loss = optimize_bound(branch.weights, branch.biases, gamma, alphas, branch.resulting_lbs, branch.resulting_ubs, num_layers, layeri, direction)  # (batch, 1)
                     assert loss.dim() == 2, loss.shape
                     assert loss.size(0) == get_num_neurons(model, layeri)
@@ -85,7 +98,6 @@ def optimize(model, H, d, num_cs, input_lbs, input_ubs, num_iters, perform_branc
                         gamma.data = torch.clamp(gamma.data, min=0)
                         for i in range(1, len(alphas)):
                             alphas[i].data = alphas[i].data.clamp(min=0.0, max=1.0)
-
             if abort:
                 break
             m, xs, zs = get_triangle_grb_model(model, branch.resulting_ubs, branch.resulting_lbs, H, d, input_lbs, input_ubs)
@@ -105,8 +117,13 @@ def optimize(model, H, d, num_cs, input_lbs, input_ubs, num_iters, perform_branc
                 abort = True
                 break
 
-            plot2d(model, H, d, approximated_input_bounds + pending_approximated_input_bounds, input_lbs, input_ubs, plot_number=plot_number, save=True, branch=branch, contour=contour)
+            plot2d(model, H, d, approximated_input_bounds + pending_approximated_input_bounds, excluded_input_regions, input_lbs, input_ubs, plot_number=plot_number, save=True, branch=branch, contour=contour)
             plot_number += 1
-        approximated_input_bounds += pending_approximated_input_bounds
-    plot2d(model, H, d, approximated_input_bounds, input_lbs, input_ubs, plot_number=plot_number, save=True, contour=contour)
+        if abort:
+            excluded_input_regions.append(ExcludedInputRegions(branch.input_lbs.cpu(), branch.input_ubs.cpu()))
+        else:
+            approximated_input_bounds += pending_approximated_input_bounds
+            if perform_branching:
+                branches += branch.split()
+    plot2d(model, H, d, approximated_input_bounds, excluded_input_regions, input_lbs, input_ubs, plot_number=plot_number, save=True, contour=contour)
     input("Press enter to terminate")

crown/plot_utils.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 import numpy as np
 import gurobipy as gp
@@ -9,7 +9,21 @@ from torch.autograd import Variable
 import core.trainer as trainer
 import matplotlib.pyplot as plt
 
-def plot2d(model: trainer.nn.Sequential, H: torch.Tensor, d: torch.Tensor, approximated_input_bounds, input_lbs, input_ubs, plot_number: int, save: bool, branch = None, contour=True):
+from crown.crown import ApproximatedInputBound, ExcludedInputRegions, InputBranch
+
+def plot2d(
+    model: trainer.nn.Sequential,
+    H: torch.Tensor,
+    d: torch.Tensor,
+    approximated_input_bounds: List[ApproximatedInputBound],
+    excluded_input_regions: List[ExcludedInputRegions],
+    input_lbs: List[torch.Tensor],
+    input_ubs: List[torch.Tensor],
+    plot_number: int,
+    save: bool,
+    branch: Optional[InputBranch] = None,
+    contour: bool =True
+):
     plt.rcParams["figure.figsize"] = (8, 8)
     plt.cla()
 
@@ -110,6 +124,17 @@ def plot2d(model: trainer.nn.Sequential, H: torch.Tensor, d: torch.Tensor, appro
         y_vals = intercept + slope * x_vals
         plt.fill(x_vals, y_vals, '--', color="red")
 
+    def get_branch_input_area_mask(input_lbs: List[torch.Tensor], input_ubs: List[torch.Tensor]):
+        ticks_per_width_x = resolution_x / (MAX_X_INPUT_VALUE - MIN_X_INPUT_VALUE)
+        ticks_per_width_y = resolution_y / (MAX_Y_INPUT_VALUE - MIN_Y_INPUT_VALUE)
+        branch_input_area_mask = np.zeros((resolution_y, resolution_x), dtype=np.bool8)
+        branch_x_lbs_in_ticks = math.ceil((input_lbs[0] - MIN_X_INPUT_VALUE) * ticks_per_width_x)
+        branch_x_ubs_in_ticks = math.floor((input_ubs[0] - MIN_X_INPUT_VALUE) * ticks_per_width_x)
+        branch_y_lbs_in_ticks = math.ceil((input_lbs[1] - MIN_Y_INPUT_VALUE) * ticks_per_width_y)
+        branch_y_ubs_in_ticks = math.floor((input_ubs[1] - MIN_Y_INPUT_VALUE) * ticks_per_width_y)
+        branch_input_area_mask[branch_y_lbs_in_ticks:branch_y_ubs_in_ticks+1, branch_x_lbs_in_ticks:branch_x_ubs_in_ticks+1] = 1
+        return branch_input_area_mask
+
     for approximated_input_bound in approximated_input_bounds:
         c = approximated_input_bound.c
         b = approximated_input_bound.b
@@ -120,14 +145,7 @@ def plot2d(model: trainer.nn.Sequential, H: torch.Tensor, d: torch.Tensor, appro
         intercept = b / c[1]
         abline(deepcopy(x_vals), deepcopy(y_vals), slope, intercept)
 
-        branch_input_area_mask = np.zeros((resolution_y, resolution_x), dtype=np.bool8)
-        ticks_per_width_x = resolution_x / (MAX_X_INPUT_VALUE - MIN_X_INPUT_VALUE)
-        ticks_per_width_y = resolution_y / (MAX_Y_INPUT_VALUE - MIN_Y_INPUT_VALUE)
-        branch_x_lbs_in_ticks = math.ceil((approximated_input_bound.input_lbs[0] - MIN_X_INPUT_VALUE) * ticks_per_width_x)
-        branch_x_ubs_in_ticks = math.floor((approximated_input_bound.input_ubs[0] - MIN_X_INPUT_VALUE) * ticks_per_width_x)
-        branch_y_lbs_in_ticks = math.ceil((approximated_input_bound.input_lbs[1] - MIN_Y_INPUT_VALUE) * ticks_per_width_y)
-        branch_y_ubs_in_ticks = math.floor((approximated_input_bound.input_ubs[1] - MIN_Y_INPUT_VALUE) * ticks_per_width_y)
-        branch_input_area_mask[branch_y_lbs_in_ticks:branch_y_ubs_in_ticks+1, branch_x_lbs_in_ticks:branch_x_ubs_in_ticks+1] = 1
+        branch_input_area_mask = get_branch_input_area_mask(approximated_input_bound.input_lbs, approximated_input_bound.input_ubs)
 
         excluded_halfspace = np.zeros((resolution_y, resolution_x), dtype=np.bool8)
         if c[1] > 0:
@@ -137,6 +155,13 @@ def plot2d(model: trainer.nn.Sequential, H: torch.Tensor, d: torch.Tensor, appro
             excluded_halfspace[intercept + XX * slope < YY] = 1
 
         remaining_input_area[branch_input_area_mask * excluded_halfspace] = 0
+
+    for excluded_input_region in excluded_input_regions:
+        branch_input_area_mask = get_branch_input_area_mask(excluded_input_region.input_lbs, excluded_input_region.input_ubs)
+        remaining_input_area[branch_input_area_mask] = 0
+        plt.plot(np.array([excluded_input_region.input_lbs[0].cpu(), excluded_input_region.input_ubs[0].cpu(), excluded_input_region.input_ubs[0].cpu(), excluded_input_region.input_lbs[0].cpu(), excluded_input_region.input_lbs[0].cpu()]),
+                 np.array([excluded_input_region.input_lbs[1].cpu(), excluded_input_region.input_lbs[1].cpu(), excluded_input_region.input_ubs[1].cpu(), excluded_input_region.input_ubs[1].cpu(), excluded_input_region.input_lbs[1].cpu()]), color="red")
+
 
     if branch is not None:
         plt.plot(np.array([branch.input_lbs[0].cpu(), branch.input_ubs[0].cpu(), branch.input_ubs[0].cpu(), branch.input_lbs[0].cpu(), branch.input_lbs[0].cpu()]),