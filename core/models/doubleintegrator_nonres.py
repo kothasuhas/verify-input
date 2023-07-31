8acbf447db8202b01e98b30e28c42e079ac95df0
Suhas Kotha <kotha.suhas@gmail.com>
2023-07-31 16:20:48 -0400

    allow for loading 3 layer NN's of arbitrary dimension

--- core/models/doubleintegrator_nonres.py
+++ core/models/doubleintegrator_nonres.py
@@ -1,42 +1,47 @@
 import torch.nn as nn
 
+STATE_DIM = 2
+HIDDEN1_DIM = 10
+HIDDEN2_DIM = 5
+POLICY_DIM = 1
+
 class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
 
-def doubleintegrator_nonres():
-    model = nn.Sequential(
-        Flatten(),
-        nn.Linear(2, 12),
-        nn.ReLU(),
-        nn.Linear(12, 7),
-        nn.ReLU(),
-        nn.Linear(7, 2)
-    )
-    return model
-
 def doubleintegrator_orig():
     model = nn.Sequential(
         Flatten(),
-        nn.Linear(2, 10),
+        nn.Linear(STATE_DIM, HIDDEN1_DIM),
         nn.ReLU(),
-        nn.Linear(10, 5),
+        nn.Linear(HIDDEN1_DIM, HIDDEN2_DIM),
         nn.ReLU(),
-        nn.Linear(5, 1)
+        nn.Linear(HIDDEN2_DIM, POLICY_DIM)
+    )
+    return model
+
+def doubleintegrator_nonres():
+    model = nn.Sequential(
+        Flatten(),
+        nn.Linear(STATE_DIM, HIDDEN1_DIM + STATE_DIM),
+        nn.ReLU(),
+        nn.Linear(HIDDEN1_DIM + STATE_DIM, HIDDEN2_DIM + STATE_DIM),
+        nn.ReLU(),
+        nn.Linear(HIDDEN2_DIM + STATE_DIM, STATE_DIM)
     )
     return model
 
 def doubleintegrator_nonres_ulimits():
     model = nn.Sequential(
         Flatten(),
-        nn.Linear(2, 12),
+        nn.Linear(STATE_DIM, HIDDEN1_DIM + STATE_DIM),
         nn.ReLU(),
-        nn.Linear(12, 7),
+        nn.Linear(HIDDEN1_DIM + STATE_DIM, HIDDEN2_DIM + STATE_DIM),
         nn.ReLU(),
-        nn.Linear(7, 3),
+        nn.Linear(HIDDEN2_DIM + STATE_DIM, POLICY_DIM + STATE_DIM),
         nn.ReLU(),
-        nn.Linear(3, 3),
+        nn.Linear(POLICY_DIM + STATE_DIM, POLICY_DIM + STATE_DIM),
         nn.ReLU(),
-        nn.Linear(3, 2),
+        nn.Linear(POLICY_DIM + STATE_DIM, STATE_DIM),
     )
     return model
---
+++ double_integrator_setup.py
@@ -0,0 +1,42 @@
+# code to convert a keras controls policy into a torch nonresidual network
+# (no need to run unless you have a new controls policy to test)
+
+import torch
+from torch import nn
+
+from keras.models import model_from_json
+
+from core.models.doubleintegrator_nonres import doubleintegrator_orig, doubleintegrator_nonres, doubleintegrator_nonres_ulimits, STATE_DIM, HIDDEN1_DIM, HIDDEN2_DIM, POLICY_DIM
+from controls_construct_fullstep import construct_full_step
+
+# assert Flatten, Linear, Relu, Linear, Relu, Linear
+
+u_limits = True
+if u_limits:
+    u_lb = torch.Tensor([-1.0])
+    u_ub = torch.Tensor([1.0])
+else:
+    u_lb, u_lb = None, None
+
+with open('doubleintegrator.json', 'r') as json_file:
+    keras_model_json = json_file.read()
+keras_model = model_from_json(keras_model_json)
+keras_model.load_weights("doubleintegrator.h5")
+
+orig_model = doubleintegrator_orig()
+if u_limits:
+    full_step = doubleintegrator_nonres_ulimits()
+else:
+    full_step = doubleintegrator_nonres()
+
+A = torch.Tensor([[1.0, 1.0], [0.0, 1.0]])
+B = torch.Tensor([[0.5], [1.0]])
+
+full_step = construct_full_step(keras_model, orig_model, full_step, 
+                                u_limits, u_lb, u_ub, 
+                                A, B,
+                                STATE_DIM, HIDDEN1_DIM, HIDDEN2_DIM, POLICY_DIM)
+if u_limits:
+    torch.save(full_step.state_dict(), 'doubleintegrator.pt')
+else:
+    torch.save(full_step.state_dict(), 'doubleintegrator_ulimits1.pt')
